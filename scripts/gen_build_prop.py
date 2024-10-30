be764cc54b412f0f0898c52b646fc792429010fb
Michael Bestas <mkbestas@lineageos.org>
2024-10-30 19:53:42 +0200

    Conditionally use Unix epoch time for build incremental
    Incremental version is used in various places to invalidate caches.
    Setting BUILD_NUMBER directly causes unnecessary rebuilds due to
    environment variable changes, so simply set it to Unix epoch unless
    BUILD_NUMBER is explicitly set.
    
    Change-Id: Id590df48ae1b73b63039f185644103d66a4bbbb3

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -121,6 +121,9 @@ def parse_args():
   config["FlareDevice"] = config["DeviceName"]
   config["FlareDesc"] = config["BuildDesc"]
 
+  if config["BuildNumber"].startswith("eng."):
+    config["BuildNumber"] = config["DateUtc"]
+
   override_config(config)
 
   append_additional_system_props(args)
