4bbe4f38c77773904bc748d445ead8ec1c0dbf5c
LuK1337 <priv.luk@gmail.com>
2025-02-19 18:36:13 +0100

    soong: Treat vendor/flare-priv/* as release-keys
    Change-Id: I7e7f180dda4402ada8b2124d0c26bb844dc2cb5f

--- android/config.go
+++ android/config.go
@@ -1376,6 +1376,9 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
+	if strings.HasPrefix(defaultCert, "vendor/flare-priv/") {
+		return "release-keys"
+	}
 	return "dev-keys"
 }
 
